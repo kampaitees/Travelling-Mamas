dcfd5b82f352002872365eb3aad7244f3e11c833
Rahul Kumar <rahul.kr0324@gmail.com>
2019-10-22 22:24:10 +0530

    added buynow html

--- dbms/dbms/settings.py
+++ dbms/dbms/settings.py
@@ -75,14 +75,12 @@ WSGI_APPLICATION = 'dbms.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mamas',
-        'USER': 'root',
-        'PASSWORD': 'Rishi$2000$%',
-        'HOST': '127.0.0.1',
-        'PORT': 3306,
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    
+}
 }
 
 
